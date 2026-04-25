ee8ea8ed3e89f7e2c7e32df2b9ef1d844410c288
Juan Esteban Vásquez Correa <juvasquezc@unal.edu.co>
2026-04-25 17:37:14 -0500

    init:initial submission

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,7 +4,7 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 # pylint: disable=import-outside-toplevel
 
-
+import pandas as pd
 def pregunta_01():
     """
     Construya y retorne un dataframe de Pandas a partir del archivo
@@ -18,3 +18,45 @@ def pregunta_01():
 
 
     """
+    with open('files/input/clusters_report.txt') as file:
+        first = file.readline().strip()
+        second = file.readline().strip()
+        aditionals = second.split("  ")
+
+        col = first.split("  ")
+        col = [s for s in col if s]
+        col[1] = col[1]+" "+aditionals[0]
+        col[2] = col[2]+" "+aditionals[1]
+        col = [c.lower().strip().replace(' ','_') for c in col]
+
+        master = []
+        mark = True
+        for line in file.readlines()[2:]:
+            record = line.strip().split("  ")
+            record = [r for r in record if r]
+            if mark:
+                c1, c2, c3, *c4 = record
+                record = [c1,c2,c3," ".join(c4)]
+                master.append(record)
+                mark = False
+            else:
+                if record:
+                    master[-1][-1] = master[-1][-1] + " "+" ".join(record)
+                else:
+                    mark = True
+        
+        df = pd.DataFrame(master,columns=col)
+        df['cluster'] = df["cluster"].astype(int)
+        df['cantidad_de_palabras_clave'] = df["cantidad_de_palabras_clave"].astype(int)
+        df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].str.replace(' %','').str.replace(',','.').astype(float)
+        df['principales_palabras_clave'] = df['principales_palabras_clave'].str.split(',')
+        df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: [' '.join(p.split()) for p in x])
+        df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: ', '.join([r.strip() for r in x]))
+        df['principales_palabras_clave'] = df['principales_palabras_clave'].str.rstrip('.')
+
+        return df
+
+print(pregunta_01())
+
+
+
